b28f6a0a9816113094477cf03bc6ef4346271256
René Kooi <renee@kooi.me>
2017-01-24 18:40:18 +0100

    Use xhr module.

--- public/javascripts/load-page.js
+++ public/javascripts/load-page.js
@@ -1,23 +1,23 @@
-/* global XMLHttpRequest */
+import xhr from 'xhr'
+
 const loading = {}
 
 export default function load (page, cb) {
   if (!loading[page]) {
     loading[page] = [ cb ]
 
-    const xhr = new XMLHttpRequest()
-    xhr.open('GET', './wiki/' + page, true)
-    xhr.addEventListener('load', function () {
-      done(null, xhr.responseText)
+    xhr(`./wiki/${page}`, (err, response) => {
+      if (err) done(err)
+      else done(null, response.body)
     })
-    xhr.addEventListener('error', done)
-    xhr.send()
   } else {
     loading[page].push(cb)
   }
 
   function done (e, content) {
-    loading[page].forEach(cb => { cb(e, content) })
+    loading[page].forEach((cb) => {
+      cb(e, content)
+    })
     delete loading[page]
   }
 }
